f042e2d642584686a133f962f06961c02909467d
Cami <caulate@estudiantec.cr>
2025-06-11 16:59:06 -0600

    funciona cat en otra consola

--- mount/mount.c
+++ mount/mount.c
@@ -8,14 +8,17 @@
 #include <string.h>
 #include <errno.h>
 #include <png.h>
+#include <limits.h>
+#include <unistd.h>
 
 #include "../include/bwfs.h"
+#include <linux/limits.h>
 
 // --------------------- GLOBALS -------------------------
 
 static Superblock sb;
 static Inode inodos[MAX_FILES];
-static char mount_folder[256];
+static char mount_folder[PATH_MAX];
 static uint8_t bitmap[MAX_BLOCKS];
 
 // --------------------- FUNCIONES AUXILIARES -------------------------
@@ -25,7 +28,7 @@ void write_struct_to_png(const char *filename, const uint8_t *data, size_t size)
 void read_data_block(int block_id, uint8_t *buffer, size_t size);
 
 void cargar_metadatos() {
-    char path[256];
+    char path[PATH_MAX];
 
     snprintf(path, sizeof(path), "%s/block_0000.png", mount_folder);
     read_struct_from_png(path, (uint8_t*)&sb, sizeof(Superblock));
@@ -46,32 +49,18 @@ void read_struct_from_png(const char *filename, uint8_t *buffer, size_t size) {
 
     png_structp png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
     png_infop info_ptr = png_create_info_struct(png_ptr);
-    if (!png_ptr || !info_ptr) {
-        fclose(fp);
-        fprintf(stderr, "Error inicializando PNG lectura\n");
-        exit(1);
-    }
-
-    if (setjmp(png_jmpbuf(png_ptr))) {
-        fclose(fp);
-        png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
-        fprintf(stderr, "Error leyendo PNG\n");
-        exit(1);
-    }
+    if (!png_ptr || !info_ptr) exit(1);
+    if (setjmp(png_jmpbuf(png_ptr))) exit(1);
 
     png_init_io(png_ptr, fp);
     png_read_info(png_ptr, info_ptr);
 
-    int width = png_get_image_width(png_ptr, info_ptr);
-    int height = png_get_image_height(png_ptr, info_ptr);
-
-    png_bytep row = malloc(width);
-    size_t bit_index = 0;
+    png_bytep row = malloc(1000);
     memset(buffer, 0, size);
-
-    for (int y = 0; y < height; y++) {
+    size_t bit_index = 0;
+    for (int y = 0; y < 1000; y++) {
         png_read_row(png_ptr, row, NULL);
-        for (int x = 0; x < width; x++) {
+        for (int x = 0; x < 1000; x++) {
             if (bit_index >= size * 8) break;
             uint8_t bit = (row[x] < 128) ? 1 : 0;
             buffer[bit_index / 8] |= (bit << (7 - (bit_index % 8)));
@@ -93,18 +82,8 @@ void write_struct_to_png(const char *filename, const uint8_t *data, size_t size)
 
     png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
     png_infop info_ptr = png_create_info_struct(png_ptr);
-    if (!png_ptr || !info_ptr) {
-        fclose(fp);
-        fprintf(stderr, "Error inicializando libpng\n");
-        exit(1);
-    }
-
-    if (setjmp(png_jmpbuf(png_ptr))) {
-        fclose(fp);
-        png_destroy_write_struct(&png_ptr, &info_ptr);
-        fprintf(stderr, "Error escribiendo PNG\n");
-        exit(1);
-    }
+    if (!png_ptr || !info_ptr) exit(1);
+    if (setjmp(png_jmpbuf(png_ptr))) exit(1);
 
     png_init_io(png_ptr, fp);
     png_set_IHDR(png_ptr, info_ptr, 1000, 1000, 8,
@@ -117,13 +96,13 @@ void write_struct_to_png(const char *filename, const uint8_t *data, size_t size)
 
     for (int y = 0; y < 1000; y++) {
         for (int x = 0; x < 1000; x++) {
+            uint8_t val = 0xFF;
             if (bit_index < size * 8) {
                 uint8_t byte = data[bit_index / 8];
                 uint8_t bit = (byte >> (7 - (bit_index % 8))) & 1;
-                row[x] = bit ? 0x00 : 0xFF;
-            } else {
-                row[x] = 0xFF;
+                val = bit ? 0x00 : 0xFF;
             }
+            row[x] = val;
             bit_index++;
         }
         png_write_row(png_ptr, row);
@@ -136,24 +115,18 @@ void write_struct_to_png(const char *filename, const uint8_t *data, size_t size)
 }
 
 void read_data_block(int block_id, uint8_t *buffer, size_t size) {
-    char path[256];
+    if (block_id < 3 || block_id >= sb.total_blocks) {
+        fprintf(stderr, "Intento de leer bloque inválido: %d\n", block_id);
+        memset(buffer, 0, size);
+        return;
+    }
+
+    char path[PATH_MAX];
     snprintf(path, sizeof(path), "%s/block_%04d.png", mount_folder, block_id);
     read_struct_from_png(path, buffer, size);
 }
 
 // --------------------- FUSE -------------------------
-
-static int fs_getattr(const char *path, struct stat *stbuf, struct fuse_file_info *fi);
-static int fs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
-                      off_t offset, struct fuse_file_info *fi, enum fuse_readdir_flags flags);
-static int fs_read(const char *path, char *buf, size_t size, off_t offset,
-                   struct fuse_file_info *fi);
-
-static struct fuse_operations fs_oper = {
-    .getattr = fs_getattr,
-    .readdir = fs_readdir,
-    .read = fs_read
-};
 
 static int fs_getattr(const char *path, struct stat *stbuf, struct fuse_file_info *fi) {
     (void) fi;
@@ -183,24 +156,18 @@ static int fs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
     (void) fi;
     (void) flags;
 
-    printf("Leyendo directorio: %s\n", path);
-
-    if (strcmp(path, "/") != 0) {
-    fprintf(stderr, "Directorio no encontrado: %s\n", path);
-    return -ENOENT;
-    }
+    if (strcmp(path, "/") != 0)
+        return -ENOENT;
 
     filler(buf, ".", NULL, 0, 0);
     filler(buf, "..", NULL, 0, 0);
 
     for (int i = 0; i < MAX_FILES; i++) {
-    if (inodos[i].used) {
-    printf(" - Encontrado archivo: %s\n", inodos[i].name);
-    filler(buf, inodos[i].name, NULL, 0, 0);
+        if (inodos[i].used)
+            filler(buf, inodos[i].name, NULL, 0, 0);
     }
-}
 
-return 0;
+    return 0;
 }
 
 static int fs_read(const char *path, char *buf, size_t size, off_t offset,
@@ -231,7 +198,6 @@ static int fs_read(const char *path, char *buf, size_t size, off_t offset,
                     to_copy = bytes_to_read - total_read;
 
                 memcpy(buf + total_read, temp_block + block_offset, to_copy);
-
                 total_read += to_copy;
                 block_index++;
                 block_offset = 0;
@@ -244,6 +210,12 @@ static int fs_read(const char *path, char *buf, size_t size, off_t offset,
     return -ENOENT;
 }
 
+static struct fuse_operations fs_oper = {
+    .getattr = fs_getattr,
+    .readdir = fs_readdir,
+    .read = fs_read,
+};
+
 // --------------------- MAIN -------------------------
 
 int main(int argc, char *argv[]) {
@@ -252,7 +224,14 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    strncpy(mount_folder, argv[1], sizeof(mount_folder));
+    // Resolver ruta absoluta
+    char real_fs_folder[PATH_MAX];
+    if (!realpath(argv[1], real_fs_folder)) {
+        perror("Error resolviendo ruta de carpeta_fs");
+        return 1;
+    }
+
+    strncpy(mount_folder, real_fs_folder, sizeof(mount_folder));
     cargar_metadatos();
 
     char *fuse_args[] = { argv[0], "-f", "-d", argv[2], NULL };
